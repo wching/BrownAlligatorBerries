a1bbaa2c0bc34c4f984aca36289d86cdb3be3e2d
Walter Ching <walter.ching.b@gmail.com>
2019-01-01 14:12:27 +0100

    Rename and move app.js to src/index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Import the appropriate service and chosen wrappers
+const {
+  dialogflow,
+  Image,
+} = require('actions-on-google')
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
